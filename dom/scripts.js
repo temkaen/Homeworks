714acd027f7aa5ea954ae6dfdba2bb911de6edfe
temkaen <temkaen@gmail.com>
2019-03-19 15:33:04 +0200

    chessUpdate

--- dom/scripts.js
+++ dom/scripts.js
@@ -33,7 +33,58 @@ var links = secondPar.getElementsByTagName("a");
                 event.stopPropagation();
         });
     }
+//Часть 2 - доска
 
+var button = document.getElementsByTagName('button')[0],
+    row = document.getElementById( "row" ),
+    col = document.getElementById( "col" ),
+    board = document.getElementsByClassName("board")[0];
 
+function createTable() {
+    var table = document.createElement("table"),
+        x = Number(row.value),
+        y = Number(col.value);
+
+    if (x>10||y>10) {
+        alert('введите число не больше 10');
+        return false;
+    }
+
+    for (var i = 1; i <= y; i++) {
+        var tr = document.createElement('tr');
+        for (var j = 1; j <= x; j++) {
+            var td = document.createElement('td');
+            (i%2 === j%2) ? td.className = "white" : td.className = "black";
+
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    board.appendChild(table);
+}
+
+function checkInput() {
+    if(row.value.length !== 0 && col.value.length !== 0 ) {
+        button.removeAttribute('disabled');
+    }
+}
+
+function reverseTd() {
+
+    var td = board.getElementsByTagName('td');
+
+    for (var i =0; i < td.length; i++) {
+        if (td[i].getAttribute('class') === 'white') {
+            td[i].className = 'black';
+        } else {
+            td[i].className = 'white';
+        }
+    }
+}
+
+button.onclick = createTable;
+row.onkeyup = checkInput;
+col.onkeyup = checkInput;
+board.onclick = reverseTd;
 
 
